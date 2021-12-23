2517ea41b877be652f4266b64419756b4713e76f
Gomam0n <603934493@qq.com>
2021-12-23 10:57:47 -0600

    basic styles and bootstrap layouts

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const path = require('path');
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
 const Campground = require('./models/campground')
-
+const ejsMate = require('ejs-mate');
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
     useNewUrlParser: true,
     useUnifiedTopology: true
@@ -20,7 +20,7 @@ db.once("open", () => {
 
 
 
-
+app.engine('ejs', ejsMate)
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname,'views'));
 app.use(express.urlencoded({ extended: true }));
