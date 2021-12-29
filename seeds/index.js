f166bc5070d8c864c6bda1e9a0cca6b626b99a0b
Gomam0n <603934493@qq.com>
2021-12-29 17:28:04 -0600

    add geocoding and map to show page

--- seeds/index.js
+++ seeds/index.js
@@ -31,6 +31,10 @@ const seedDB = async () => {
             //image: 'https://source.unsplash.com/collection/483251',
             description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam dolores vero perferendis laudantium, consequuntur voluptatibus nulla architecto, sit soluta esse iure sed labore ipsam a cum nihil atque molestiae deserunt!',
             price,
+            geometry: {
+                type: "Point",
+                coordinates: [-113.1331, 47.0202]
+            },
             images: [
                 {
                     url: 'https://res.cloudinary.com/douqbebwk/image/upload/v1600060601/YelpCamp/ahfnenvca4tha00h2ubt.png',
