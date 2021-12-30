5bd293ac061d942bb3900304e5a21d8e03fa496c
Gomam0n <603934493@qq.com>
2021-12-29 21:52:33 -0600

    add fancy cluster map

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -3,15 +3,19 @@ const mbxGeocoding = require("@mapbox/mapbox-sdk/services/geocoding");
 const mapBoxToken = process.env.MAPBOX_TOKEN;
 const geocoder = mbxGeocoding({ accessToken: mapBoxToken });
 const { cloudinary } = require("../cloudinary");
+
+
 module.exports.index = async (req, res) => {
-    const campgrounds = await Campground.find({});
+    const campgrounds = await Campground.find({}).populate({
+        path: 'popupText',
+        strictPopulate: false,  // strictPopulate needs to be added
+});
     res.render('campgrounds/index', { campgrounds })
 }
 
 module.exports.renderNewForm = (req, res) => {
     res.render('campgrounds/new');
 }
-
 module.exports.createCampground = async (req, res, next) => {
     const geoData = await geocoder.forwardGeocode({
         query: req.body.campground.location,
@@ -26,7 +30,6 @@ module.exports.createCampground = async (req, res, next) => {
     req.flash('success', 'Successfully made a new campground!');
     res.redirect(`/campgrounds/${campground._id}`)
 }
-
 module.exports.showCampground = async (req, res,) => {
     const campground = await Campground.findById(req.params.id).populate({
         path: 'reviews',
@@ -40,7 +43,6 @@ module.exports.showCampground = async (req, res,) => {
     }
     res.render('campgrounds/show', { campground });
 }
-
 module.exports.renderEditForm = async (req, res) => {
     const { id } = req.params;
     const campground = await Campground.findById(id)
@@ -50,7 +52,6 @@ module.exports.renderEditForm = async (req, res) => {
     }
     res.render('campgrounds/edit', { campground });
 }
-
 module.exports.updateCampground = async (req, res) => {
     const { id } = req.params;
     console.log(req.body);
@@ -67,7 +68,6 @@ module.exports.updateCampground = async (req, res) => {
     req.flash('success', 'Successfully updated campground!');
     res.redirect(`/campgrounds/${campground._id}`)
 }
-
 module.exports.deleteCampground = async (req, res) => {
     const { id } = req.params;
     await Campground.findByIdAndDelete(id);
--- models/campground.js
+++ models/campground.js
@@ -2,15 +2,16 @@ const mongoose = require('mongoose');
 const Review = require('./review')
 const Schema = mongoose.Schema;
 // https://res.cloudinary.com/douqbebwk/image/upload/w_300/v1600113904/YelpCamp/gxgle1ovzd2f3dgcpass.png
-
 const ImageSchema = new Schema({
     url: String,
     filename: String
 });
-
 ImageSchema.virtual('thumbnail').get(function () {
     return this.url.replace('/upload', '/upload/w_200');
 });
+
+const opts = { toJSON: { virtuals: true } };
+
 const CampgroundSchema = new Schema({
     title: String,
     images: [ImageSchema],
@@ -38,7 +39,15 @@ const CampgroundSchema = new Schema({
             ref: 'Review'
         }
     ]
+}, opts);
+
+
+CampgroundSchema.virtual('properties.popUpMarkup').get(function () {
+    return `
+    <strong><a href="/campgrounds/${this._id}">${this.title}</a><strong>
+    <p>${this.description.substring(0, 20)}...</p>`
 });
+
 
 CampgroundSchema.post('findOneAndDelete', async function (doc) {
     if (doc) {
@@ -49,5 +58,4 @@ CampgroundSchema.post('findOneAndDelete', async function (doc) {
         })
     }
 })
-
 module.exports = mongoose.model('Campground', CampgroundSchema);
